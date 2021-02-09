b5cc2ffe156c3357e0cc2df986e93ba32c25a601
Delagros <martin2003zak@seznam.cz>
2021-02-09 18:50:08 +0100

    ...

---
+++ Zaklady_OOP_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaklady_OOP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
